072e6cf77e9131a3ef521bf1e2b4dbdde74bb9b1
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-10-23 15:55:55 +0200

    working contact section

---
+++ index.js
@@ -0,0 +1,11 @@
+function sendEmail(){
+ window.location= "mailto:tomek.woytkowiak@gmail.com"
+}
+function openLinkedin(){
+ var url = "https://www.linkedin.com/in/tomek-woytkowiak/";
+window.open(url, '_blank').focus();
+}
+function openGithubProfile(){
+ var url = "https://github.com/Scoobydowsky";
+ window.open(url, '_blank').focus();
+}
