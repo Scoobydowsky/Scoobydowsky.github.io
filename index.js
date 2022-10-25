f6db10f05d61a8b68c5be1af8f8e52ae9dd22ffa
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-10-25 12:28:07 +0200

    started working on translation

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-function sendEmail(){
- window.location= "mailto:tomek.woytkowiak@gmail.com"
-}
-function openLinkedin(){
- var url = "https://www.linkedin.com/in/tomek-woytkowiak/";
-window.open(url, '_blank').focus();
-}
-function openGithubProfile(){
- var url = "https://github.com/Scoobydowsky";
- window.open(url, '_blank').focus();
-}
-function downloadCV(){
+var lang = "en";
 
+function setLangPL() {
+    lang = "pl";
+    console.log("Ustawiono jezyk na polski");
+    document.documentElement.setAttribute("lang", 'pl');
+    //TODO load translate
+}
+
+function setLangEN() {
+    lang = "en"
+    console.log("Set lang to en");
+    document.documentElement.setAttribute("lang", 'en');
+    //TODO load translate
+}
+
+function sendEmail() {
+    window.location = "mailto:tomek.woytkowiak@gmail.com"
+}
+
+function openLinkedin() {
+    var url = "https://www.linkedin.com/in/tomek-woytkowiak/";
+    window.open(url, '_blank').focus();
+}
+
+function openGithubProfile() {
+    var url = "https://github.com/Scoobydowsky";
+    window.open(url, '_blank').focus();
+}
+
+function downloadCV() {
+    var url = "";
+    if (lang == "pl") {
+        url = "/res/cv-pl.pdf";
+    } else {
+        url = "/res/cv-en.pdf";
+    }
+    window.open(url, '_blank').focus();
 }
