888d17ad1c4288e3001465f571b2a2f836aecd51
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-10-29 00:04:56 +0200

    translation in progress 2/3 completed

--- index.js
+++ index.js
@@ -30,7 +30,28 @@ function setLangPL() {
     document.getElementById("skills-section-skill2-dsc").innerHTML="Opis co umiem";//TODO
     document.getElementById("skills-section-skill3-dsc").innerHTML="Opis co umiem";//TODO
     document.getElementById("skills-section-skill4-dsc").innerHTML="Opis co umiem";//TODO
-
+    document.getElementById("skills-section-skill5-dsc").innerHTML="Opis co umiem";//TODO
+    document.getElementById("skills-section-skill6-dsc").innerHTML="Opis co umiem";//TODO
+    /* education section translate */
+    document.getElementById("education-section-desciption").innerHTML="Wykształcenie";
+    document.getElementById("education-section-school2-date").setAttribute("data-date","2021-obecnie");
+    document.getElementById("education-section-school2-title").innerHTML="Inżynier Informatyk";
+    document.getElementById("education-section-school2-name").innerHTML="Collegium Da Vinci w Poznaniu";
+    document.getElementById("education-section-school1-date").setAttribute("data-date","2013-2017");
+    document.getElementById("education-section-school1-title").innerHTML="Technik Informatyk";
+    document.getElementById("education-section-school1-name").innerHTML="Zespół Szkół Technicznych w Lesznie";
+    /* experience section translate */
+    document.getElementById("experience-section-label").innerHTML="Doświadczenie";
+    document.getElementById("experience-section-work1-date").setAttribute("data-date","N/A");
+    document.getElementById("experience-section-work1-job").innerHTML="Programista PHP";
+    document.getElementById("experience-section-work1-company").innerHTML="Brak Firmy";
+    document.getElementById("experience-section-work1-dsc").innerHTML="Nie mam jeszcze żadnego doświadczenia";
+    /*certification section translate */
+    document.getElementById("certification-section-label").innerHTML="Certyfikaty";
+    document.getElementById("certification-section-certificate1-name").innerHTML="Nazwa Certyfikatu";
+    document.getElementById("certification-section-certificate1-company").innerHTML="Firma wystawiajace certyfikat";
+    document.getElementById("certification-section-certificate1-check").innerHTML="Zobacz Certifikat";
+    /*project section translate */
 }
 
 function setLangEN() {
