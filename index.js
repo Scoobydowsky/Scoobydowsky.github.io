f268ce1301e599b370d5e0abba7fa1594adfb016
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-10-29 09:52:46 +0200

    translation to pl is end

--- index.js
+++ index.js
@@ -53,13 +53,75 @@ function setLangPL() {
     document.getElementById("certification-section-certificate1-company").innerHTML="Firma wystawiajace certyfikat";
     document.getElementById("certification-section-certificate1-check").innerHTML="Zobacz Certifikat";
     /*project section translate */
+    document.getElementById("project-section-label").innerHTML = "Projekty";
+    document.getElementById("project-section-project1-name").innerHTML = "Projekt1";
+    document.getElementById("project-section-project2-name").innerHTML = "Projekt2";
+    document.getElementById("project-section-project3-name").innerHTML = "Projekt2";
+
+    /* Contact Section translate*/
+    document.getElementById("contact-section-label").innerHTML="Kontakt";
 }
 
 function setLangEN() {
     lang = "en"
     console.log("Set lang to en");
     document.documentElement.setAttribute("lang", 'en');
-    //TODO load translate
+    /* Navbar translation*/
+    document.getElementById("about-me-link").innerHTML="About me";
+    document.getElementById("skills-link").innerHTML="Skills";
+    document.getElementById("education-link").innerHTML="Education";
+    document.getElementById("experience-link").innerHTML="Experience";
+    document.getElementById("certification-link").innerHTML="Certifications";
+    document.getElementById("project-link").innerHTML="Project";
+    document.getElementById("contact-link").innerHTML="Contact";
+    /*Hello section translation */
+    document.getElementById("hello-section-welcome").innerHTML="Hi, im Tomek";
+    document.getElementById("hello-section-description").innerHTML="PHP Developer";
+    /* about me section translation */
+    document.getElementById("aboutMe-section-label").innerHTML="About Me";
+    document.getElementById("aboutMe-section-description").innerHTML="Witaj, nazywam się Tomasz Woytkowiak. Studiuję informatykę na Collegium Da vinci w Poznaniu.\n" +
+        "                        W swoich projektach wykorzystuje PHP , obecnie uczę się wykorzystania Frameworku Symfony 5.\n" +
+        "                        Hobbystycznie tworzę różne rzeczy przy wykorzystaniu Arduino.";
+    document.getElementById("aboutMe-section-hobby-label").innerHTML="Hobby:";
+    document.getElementById("aboutMe-section-hobby1").innerHTML="Programowanie";
+    document.getElementById("aboutMe-section-hobby2").innerHTML="Gry komputerowe";
+    document.getElementById("aboutMe-section-hobby3").innerHTML="Numizmatyka";
+    /* skills section translate*/
+    document.getElementById("skills-section-label").innerHTML="Umiejętności";
+    //TODO switch skills id to more accurate
+    document.getElementById("skills-section-skill1-dsc").innerHTML="Opis co umiem";//TODO
+    document.getElementById("skills-section-skill2-dsc").innerHTML="Opis co umiem";//TODO
+    document.getElementById("skills-section-skill3-dsc").innerHTML="Opis co umiem";//TODO
+    document.getElementById("skills-section-skill4-dsc").innerHTML="Opis co umiem";//TODO
+    document.getElementById("skills-section-skill5-dsc").innerHTML="Opis co umiem";//TODO
+    document.getElementById("skills-section-skill6-dsc").innerHTML="Opis co umiem";//TODO
+    /* education section translate */
+    document.getElementById("education-section-desciption").innerHTML="Wykształcenie";
+    document.getElementById("education-section-school2-date").setAttribute("data-date","2021-obecnie");
+    document.getElementById("education-section-school2-title").innerHTML="Inżynier Informatyk";
+    document.getElementById("education-section-school2-name").innerHTML="Collegium Da Vinci w Poznaniu";
+    document.getElementById("education-section-school1-date").setAttribute("data-date","2013-2017");
+    document.getElementById("education-section-school1-title").innerHTML="Technik Informatyk";
+    document.getElementById("education-section-school1-name").innerHTML="Zespół Szkół Technicznych w Lesznie";
+    /* experience section translate */
+    document.getElementById("experience-section-label").innerHTML="Doświadczenie";
+    document.getElementById("experience-section-work1-date").setAttribute("data-date","N/A");
+    document.getElementById("experience-section-work1-job").innerHTML="Programista PHP";
+    document.getElementById("experience-section-work1-company").innerHTML="Brak Firmy";
+    document.getElementById("experience-section-work1-dsc").innerHTML="Nie mam jeszcze żadnego doświadczenia";
+    /*certification section translate */
+    document.getElementById("certification-section-label").innerHTML="Certyfikaty";
+    document.getElementById("certification-section-certificate1-name").innerHTML="Nazwa Certyfikatu";
+    document.getElementById("certification-section-certificate1-company").innerHTML="Firma wystawiajace certyfikat";
+    document.getElementById("certification-section-certificate1-check").innerHTML="Zobacz Certifikat";
+    /*project section translate */
+    document.getElementById("project-section-label").innerHTML = "Projekty";
+    document.getElementById("project-section-project1-name").innerHTML = "Projekt1";
+    document.getElementById("project-section-project2-name").innerHTML = "Projekt2";
+    document.getElementById("project-section-project3-name").innerHTML = "Projekt2";
+
+    /* Contact Section translate*/
+    document.getElementById("contact-section-label").innerHTML="Kontakt";
 }
 
 function sendEmail() {
